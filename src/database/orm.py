a8714c1c6ea45f43354ce4f12f77be5e27fd1132
bess-cater <caterlilu@gmail.com>
2025-07-23 17:19:41 +0900

    🗃️ fix: kakao / 일반 유저 테이블 분리 +refreshToken 수정

--- src/database/orm.py
+++ src/database/orm.py
@@ -6,22 +6,40 @@ from datetime import datetime
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(255), unique=True, nullable=True) #for local only.
+    email = Column(String(255), unique=True, nullable=False)
+    hashed_password = Column(String(255), nullable=True)  #NB:  does not exist in kakao!
     name = Column(String(255), nullable=False)
-    email = Column(String(255), nullable=False, unique=True)
-    nickname = Column(String(255), nullable=True)
-    profile_photo = Column(String(255), nullable=True)
+    auth_provider = Column(Enum('local', 'kakao', name='auth_provider'), nullable=False, default='local')
     intro = Column(Text, nullable=True)
     language_cd = Column(Enum('KO', 'EN', 'JP', 'CN', 'FR', 'RU', name='language_cd'), nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
-    refresh_tokens = relationship('RefreshToken', back_populates='user')
+
+    kakao = relationship("KakaoUser", back_populates="user", uselist=False)
+
+
+class KakaoUser(Base):
+    __tablename__ = 'kakao_user'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, unique=True)
+
+    nickname = Column(String(255), nullable=True)
+    profile_photo = Column(String(255), nullable=True)
+
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+
+    user = relationship("User", back_populates="kakao")
 
 class RefreshToken(Base):
     __tablename__ = 'refresh_token'
+    
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
     token = Column(String(512), nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-    user = relationship('User', back_populates='refresh_tokens') 
+
+    user = relationship("User", backref="refresh_tokens")
