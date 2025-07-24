972bc9c4e6a6d202ace27111b392950be5fd8721
syyling <sy0hiu@gmail.com>
2025-07-24 23:23:41 +0900

    :recycle: travel 관련 orm 파일 분리

--- src/database/orm.py
+++ src/database/orm.py
@@ -21,14 +21,14 @@ class User(Base):
     age = Column(Integer, nullable=True)
     thema = Column(Text, nullable=True)
 
-
+    refresh_tokens = relationship("RefreshToken", back_populates="user", cascade="all, delete-orphan")
     kakao = relationship("KakaoUser", back_populates="user", uselist=False, cascade='all, delete-orphan')
 
 
 class KakaoUser(Base):
     __tablename__ = 'kakao_user'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, unique=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False, unique=True)
 
     nickname = Column(String(255), nullable=True)
     profile_photo = Column(String(255), nullable=True)
@@ -39,36 +39,7 @@ class RefreshToken(Base):
     __tablename__ = 'refresh_token'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False, index=True)
     token = Column(String(512), nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     user = relationship('User', back_populates='refresh_tokens')
-
-
-class TravelLog(Base):
-    __tablename__ = "travel_log"
-    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    title = Column(String(256), nullable=False)
-    user_id = Column(Integer, ForeignKey("user.id")) # 외래키
-
-class TravelLocation(Base) :
-    __tablename__ = "travel_location"
-    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    travel_log_id = Column(Integer, ForeignKey("travel_log.id"))  # 외래키
-    user_id = Column(Integer, ForeignKey("user.id")) # 외래키
-    name = Column(String(256), nullable=False)
-    sequence = Column(Integer, nullable=False)
-    longitude = Column(Float, nullable=False)
-    latitude = Column(Float, nullable=False)
-    congestion = Column(String(256), nullable=False)
-
-class TravelStamp(Base) :
-    __tablename__ = "travel_stamp"
-    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    travel_log_id = Column(Integer, ForeignKey("travel_log.id"))  # 외래키
-    travel_location_id = Column(Integer, ForeignKey("travel_location.id"))  # 외래키
-    user_id = Column(Integer, ForeignKey("user.id")) # 외래키
-    title = Column(String(256), nullable=False)
-    emotion = Column(String(256), nullable=False)
-    isStamped = Column(Boolean, nullable=False)
-    stamped_at = Column(DateTime, nullable=False)
